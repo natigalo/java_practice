c9e45c5da5ac875feb16807feb6fb76978ff48f6
natigalo <natigalo1@gmail.com>
2023-07-09 08:14:13 -0500

    ejercicio 1

---
+++ Java-endpoint/src/test/java/com/example/Javaendpoint/JavaEndpointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Javaendpoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaEndpointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
